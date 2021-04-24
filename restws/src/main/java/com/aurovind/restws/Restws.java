ff38aa249e2dd7f3661e74925b25074545ed2188
aurovindsamal <77598964+aurovindsamal@users.noreply.github.com>
2021-04-24 13:00:50 +0530

    Add files via upload

---
+++ restws/src/main/java/com/aurovind/restws/Restws.java
@@ -0,0 +1,13 @@
+package com.aurovind.restws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restws {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restws.class, args);
+	}
+
+}
---
+++ restws/src/main/java/com/aurovind/restws/exceptions/PatientBusinessException.java
@@ -0,0 +1,6 @@
+package com.aurovind.restws.exceptions;
+
+public class PatientBusinessException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+}
